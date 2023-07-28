992bd2710d00068b99bda66c7b7c7eea0dda8fc6
kaggelos <aggelosk17@hotmail.com>
2023-07-28 18:36:36 +0300

    Add JavaScript and Active CSS

---
+++ js/main.js
@@ -0,0 +1,25 @@
+async function populateList(url) {
+    fetch(url).then((response) => response.json()).then((array) => {
+    let sum = 0;
+    for (const x of array) {
+        document.getElementById("subject-list").innerHTML += 
+        '<li style="background-color: hsla(' + x.color + ', 10%); color: hsl('  + x.color + ')"><img class="subject-icon" src='+ x.icon +'></img>'+
+        '<div class="subject" id="' + x.category + '">' + x.category + '</div>' + 
+        '<div class="subject-grade">' + '<span>' + x.score + '</span> / 100</div></li>';
+        sum += x.score;
+    };
+    const grade = sum/array.length;
+    document.getElementById('grade').innerHTML = Math.floor(grade);
+    const titles = [
+        "Bad",
+        "Average",
+        "Good",
+        "Great"
+    ];
+    for (let i = 0; grade > (100/titles.length)*i; i++) {
+        document.getElementById('title').innerHTML = titles[i];
+    }
+  });
+};
+
+populateList("./js/json/data.json");
